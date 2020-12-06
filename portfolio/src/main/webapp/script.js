cb37b024f2f6b5065ff9682ed1d265b913b92474
Anthony869 <anthony@achille.com>
2020-12-06 23:43:25 +0000

    fixed the 2 spacing issues and the remaining code that didn't do anyhting in the example

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -27,8 +27,9 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
-  async function getHello() {
+  //Uses fetch to get content from servlet and puts it in hello container
+async function getHello() {
   const response = await fetch('/data');
   const quote = await response.text();
   document.getElementById('hello-container').innerText = quote;
-  }
+}
